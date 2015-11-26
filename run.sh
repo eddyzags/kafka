c0d107b912743f5c7ee0e36288305f4548ff65f3
Eddy Zagabe <zagabe.ed@gmail.com>
2015-11-26 15:47:30 +0100

    Add launch script for Apache Mesos container

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export LIBPROCESS_IP=$(ip a show dev eth0 | grep inet | grep eth0 | sed -e 's/^.*inet.//g' -e 's/\/.*$//g')
+
+$(pwd)/kafka-mesos.sh scheduler --api http://$LIBPROCESS_IP:7002 $@
