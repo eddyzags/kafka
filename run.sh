cee3300983a238ab32552f1ad7ac2a459131ae88
Eddy Zagabe <zagabe.ed@gmail.com>
2015-11-26 15:56:29 +0100

    run.sh launch path changed

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 export LIBPROCESS_IP=$(ip a show dev eth0 | grep inet | grep eth0 | sed -e 's/^.*inet.//g' -e 's/\/.*$//g')
 
-$(pwd)/kafka-mesos.sh scheduler --api http://$LIBPROCESS_IP:7002 $@
+$(pwd)/kafka-master/kafka-mesos.sh scheduler --api http://$LIBPROCESS_IP:7002 $@
