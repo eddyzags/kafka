5a86cc2f17e2a3eb1df38da9b11183f36b7acef1
Eddy Zagabe <zagabe.ed@gmail.com>
2016-05-22 00:20:08 +0200

    bind-address parameter added

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 export LIBPROCESS_IP=$(ip a show dev eth0 | grep inet | grep eth0 | sed -e 's/^.*inet.//g' -e 's/\/.*$//g')
 
-echo $LIBPROCESS_UP
+echo $LIBPROCESS_IP
 
-$(pwd)/kafka-mesos.sh scheduler --api http://$LIBPROCESS_IP:$PORT0 $@
+$(pwd)/kafka-mesos.sh scheduler --api http://$LIBPROCESS_IP:$PORT0 --bind-address $LIBPROCESS_IP $@
