33a52ee1b28ac14a9596b7a0058b7a4ea10f6104
Eddy Zagabe <zagabe.ed@gmail.com>
2016-05-22 00:10:59 +0200

    debug

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 
 export LIBPROCESS_IP=$(ip a show dev eth0 | grep inet | grep eth0 | sed -e 's/^.*inet.//g' -e 's/\/.*$//g')
 
+echo $LIBPROCESS_UP
+
 $(pwd)/kafka-mesos.sh scheduler --api http://$LIBPROCESS_IP:$PORT0 $@
